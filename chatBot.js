1202fee47b2708f0d7992cdc11b196aed7b58552
Donnie Plumly <donnie.plumly@gmail.com>
2019-11-01 13:07:14 -0400

    reply, translation
    basic ask and reply from bot and chinese to english card translation

---
+++ chatBot.js
@@ -0,0 +1,86 @@
+
+// Chinese translator
+function createCardResponse(widgets) {
+  return {
+    "cards": [
+      {
+        "sections": [{
+          "widgets": widgets
+        }]
+    }]
+  };
+}
+
+function onMessage(e) {
+  var sourceText = e.message.argumentText;
+  var translatedText = "Chinese:" + LanguageApp.translate(sourceText, 'en', 'zh-CN');
+  var widgets = [{
+    "textParagraph": {
+      "text": translatedText}
+  }];
+  return createCardResponse(widgets);
+}
+
+
+
+
+
+/*
+ * Responds to a MESSAGE event in Hangouts Chat.
+ * @param {Object} event the event object from Hangouts Chat
+ */
+ //Setting the names of messangers
+function onMessage(event) {
+    var name = "";
+  
+    //Setting the name for the incoming message
+    if (event.space.type == "DM") {
+      name = "You";
+    } else {
+      name = event.user.displayName;
+    }
+    
+    // This is the structure to reply looking for certain words
+    var message = name + " said \"" + event.message.text + "\"";
+    var outSick = "feel better";
+    if (message.indexOf("out sick")>=0) {
+      return { "text": outSick };
+    }
+  }
+  
+    
+       
+       
+       
+       
+       
+       
+       
+       
+       
+ // 
+  function onAddToSpace(event) {
+    var message = "";
+  
+    if (event.space.type == "DM") {
+      message = "Thank you for adding me to a DM, " + event.user.displayName + "!";
+    } else {
+      message = "Thank you for adding me to " + event.space.displayName;
+    }
+  
+    if (event.message) {
+      // Bot added through @mention.
+      message = message + " and you said: \"" + event.message.text + "\"";
+    }
+    return { "text": message };
+  }
+  
+  /*
+   * Responds to a REMOVED_FROM_SPACE event in Hangouts Chat.
+   * @param {Object} event the event object from Hangouts Chat
+   */
+  function onRemoveFromSpace(event) {
+    console.info("Bot removed from ", event.space.name);
+  }
+  
+  
